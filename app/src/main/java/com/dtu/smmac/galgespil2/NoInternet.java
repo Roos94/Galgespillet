c509456470bd082c74d658392456113f71539f43
SteenBartholdy <s145090@student.dtu.dk>
2016-01-05 11:56:20 +0100

    Tjek af internetforbindelse

--- app/src/main/java/com/dtu/smmac/galgespil2/NoInternet.java
+++ app/src/main/java/com/dtu/smmac/galgespil2/NoInternet.java
@@ -3,13 +3,14 @@ package com.dtu.smmac.galgespil2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class NoInternet extends Activity implements View.OnClickListener {
+public class NoInternet extends Activity {
 
-    private Button accept, close;
     private TextView noInternet;
     private Intent i;
 
@@ -18,34 +19,26 @@ public class NoInternet extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_internet);
 
-        accept = (Button) findViewById(R.id.acceptB);
+        this.noInternet = (TextView) findViewById(R.id.noInternet);
 
-        close = (Button) findViewById(R.id.close);
-
-        noInternet = (TextView) findViewById(R.id.noInternet);
-
-
-        accept.setOnClickListener(this);
-
-        close.setOnClickListener(this);
-
-        noInternet.setText("Du har ikke forbindelse til internette og vil derfor ikke få det fulde ud af denne applikation." + "\n" +
+        this.noInternet.setText("Du har ikke forbindelse til internette og vil derfor ikke få det fulde ud af denne applikation." + "\n" +
                 "Vil du fortsætte med at bruge denne applikation med en mindre ordliste?");
 
-        i = new Intent(this, Main.class);
+        this.i = new Intent(this, Main.class);
     }
 
     @Override
-    public void onClick(View v) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_topbar, menu);
 
-        if(v == accept) {
-
-            startActivity(i);
-        }
-        else if (v == close)
-        {
-            finish();
-            System.exit(0);
-        }
+        return super.onCreateOptionsMenu(menu);
     }
+
+    public void done(MenuItem item)
+    {
+        startActivity(i);
+    }
+
+
+
 }
